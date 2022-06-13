4680e7acb4f4bd580199ab044073615034d6b0f6
Eduardo S. Mesquita <102036752+Duzoka@users.noreply.github.com>
2022-06-13 16:37:44 -0300

    Add files via upload

---
+++ javascript/indexpag.js
@@ -0,0 +1,71 @@
+
+
+/* Animações de botões */
+
+$(".main-pag-home-icon").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#tohru-apresentation").offset().top
+  }, );
+});
+
+$(".tohru-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#tohru-session").offset().top
+    },);
+  });
+
+  $(".kobayashi-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#kobayashi-session").offset().top
+    },);
+  });
+
+  $(".kanna-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#kanna-session").offset().top
+    },);
+  });
+
+  $(".elma-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#elma-session").offset().top
+    },);
+  });
+
+  $(".fafnir-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#fafnir-session").offset().top
+    },);
+  });
+
+  $(".saikawa-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#saikawa-session").offset().top
+    },);
+  });
+
+  $(".shouta-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#shouta-session").offset().top
+    },);
+  });
+
+  $(".lucoa-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#quetza-session").offset().top
+    },);
+  });
+
+  $(".ilulu-home-icon").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#ilulu-session").offset().top
+    },);
+  });
+
+  /* Fim da animação dos botões */
+
+$(".icon-navbar").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#tohru-apresentation").offset().top
+    },);
+  });
