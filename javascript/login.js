23201eec31fdb35dd34ee3f782f957281efa671a
Du <102036752+Duzoka@users.noreply.github.com>
2022-06-15 11:46:40 -0300

    removed the test

--- javascript/login.js
+++ javascript/login.js
@@ -1,10 +1,22 @@
 function logar () {
+
+    let usernameA = {
+        'Duzoka':
+        321,
+        'Admin':
+        123,
+        'Eduardo':
+        432,
+        'Dudu':
+        111,
+    }
+
     var username = document.getElementById("usuario");
     var senha = document.getElementById("senha");
 
     console.log(username.value+senha.value);
 
-    if (username.value = "admin" && senha.value == "123") {
+    if (username.value = usernameA && senha.value == usernameA) {
         localStorage.setItem("acesso", true);
 
         window.location.href = ("mainpag.html");
